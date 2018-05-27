02401b16ab00cd68ea1f76fa9899f2ac96674613
Aliaksandr Rykau <Aliaksandr_Rykau@epam.com>
2018-05-27 20:57:26 +0300

    Merge step3. Add ProductCount to ProductCategoryListItem model. Get ProductCount evaluation logic to ProductCategoryService.GetCategoriesAsync.

--- KatlaSport.Services/ProductManagement/ProductCategoryService.cs
+++ KatlaSport.Services/ProductManagement/ProductCategoryService.cs
@@ -34,7 +34,10 @@ namespace KatlaSport.Services.ProductManagement
             var dbCategories = await _context.Categories.OrderBy(c => c.Id).Skip(start).Take(amount).ToListAsync();
             var categories = dbCategories.Select(c => Mapper.Map<ProductCategoryListItem>(c)).ToList();
 
-            // TODO STEP 3 - Add code for setting "ProductCount" property for each category here.
+            foreach (var c in categories)
+            {
+                c.ProductCount = await _context.Products.Where(p => p.CategoryId == c.Id).CountAsync();
+            }
 
             return categories;
         }
