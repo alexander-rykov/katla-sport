b129c3c544ac8637cb2557a8fa158c6485446888
Aliaksandr Rykau <aliaksandr_rykau@epam.com>
2018-06-04 16:27:31 +0300

    Merge step10. Add Delete/Restore functionality to hive-list. Add Delete/Restore functionality to hive-section-list.

--- KatlaSportNg/src/app/hive-management/services/hive-section.service.ts
+++ KatlaSportNg/src/app/hive-management/services/hive-section.service.ts
@@ -22,6 +22,6 @@ export class HiveSectionService {
   }
 
   setHiveSectionStatus(hiveSectionId: number, deletedStatus: boolean): Observable<Object> {
-    return null;
+    return this.http.put(`${this.url}${hiveSectionId}/status/${deletedStatus}`, null);
   }
 }
--- KatlaSportNg/src/app/hive-management/services/hive.service.ts
+++ KatlaSportNg/src/app/hive-management/services/hive.service.ts
@@ -39,6 +39,6 @@ export class HiveService {
   }
 
   setHiveStatus(hiveId: number, deletedStatus: boolean): Observable<Object> {
-    return null;
+    return this.http.put(`${this.url}${hiveId}/status/${deletedStatus}`, null);
   }
 }
