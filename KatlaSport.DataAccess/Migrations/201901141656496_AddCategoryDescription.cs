38200acf8f3aacfe284fa9c25563026dce4d205d
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2019-01-14 20:02:34 +0300

    Updare Step1
    AddCategoryDescription xml added

--- KatlaSport.DataAccess/Migrations/201901141656496_AddCategoryDescription.cs
+++ KatlaSport.DataAccess/Migrations/201901141656496_AddCategoryDescription.cs
@@ -1,15 +1,17 @@
 namespace KatlaSport.DataAccess.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
+    /// <summary>
+    /// Represents AddCategoryDescription migration.
+    /// </summary>
     public partial class AddCategoryDescription : DbMigration
     {
         public override void Up()
         {
             AddColumn("dbo.product_categories", "category_description", c => c.String(maxLength: 300));
         }
-        
+
         public override void Down()
         {
             DropColumn("dbo.product_categories", "category_description");
