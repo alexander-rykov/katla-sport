0b1bc09a4bd461eed7caed41b7f352eecee6c8f4
Артем Лыщик <artsiom.lyshchyk@gmail.com>
2019-01-17 21:11:54 +0300

    step_6_finished

--- KatlaSportNg/src/app/hive-management/models/hive-list-item.ts
+++ KatlaSportNg/src/app/hive-management/models/hive-list-item.ts
@@ -1,7 +1,8 @@
 export class HiveListItem {
     constructor(
         public id: number,
-        // TODO STEP 6 - Add parameters name and code here.
+        public name: string,
+        public code: string,
         public isDeleted: boolean
     ) { }
 }
