cfc6cf870f2784f8fe70239cfee72b151c4354ff
Alexander Ataakgayev <bad.bad.jaguar@gmail.com>
2019-01-18 14:02:08 +0300

    Add UpdateHiveSectionRequest

---
+++ KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequest.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.Attributes;
+
+namespace KatlaSport.Services.HiveManagement
+{
+    /// <summary>
+    /// Represents a request for creating and updating a hive section.
+    /// </summary>
+    public class UpdateHiveSectionRequest
+    {
+        /// <summary>
+        /// Gets or sets a store hive name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a store hive code.
+        /// </summary>
+        public string Code { get; set; }
+    }
+}
