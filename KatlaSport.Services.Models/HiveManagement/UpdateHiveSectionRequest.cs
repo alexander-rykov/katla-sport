49637bc526f49db162485957cbf0faa0bbcde9cc
Alexander Ataakgayev <bad.bad.jaguar@gmail.com>
2019-01-18 14:59:03 +0300

    Fillerd classes
    Filled classes UpdateHiveSectionRequestValidator and UpdateHiveSectionRequest.

--- KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequest.cs
+++ KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequest.cs
@@ -1,8 +1,12 @@
-﻿namespace KatlaSport.Services.HiveManagement
+﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation.Attributes;
+
+namespace KatlaSport.Services.HiveManagement
 {
     /// <summary>
     /// Represents a request for creating and updating a hive section.
     /// </summary>
+    [Validator(typeof(UpdateHiveSectionRequestValidator))]
     public class UpdateHiveSectionRequest
     {
         /// <summary>
--- KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequestValidator.cs
+++ KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequestValidator.cs
@@ -1,7 +1,19 @@
-﻿namespace KatlaSport.Services.HiveManagement
-{
-    public class UpdateHiveSectionRequestValidator
-    {
+﻿using FluentValidation;
 
+namespace KatlaSport.Services.HiveManagement
+{
+    /// <summary>
+    /// Represents a validator for <see cref="UpdateHiveSectionRequestValidator"/>
+    /// </summary>
+    public class UpdateHiveSectionRequestValidator : AbstractValidator<UpdateHiveSectionRequest>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdateHiveSectionRequestValidator"/> class.
+        /// </summary>
+        public UpdateHiveSectionRequestValidator()
+        {
+            RuleFor(r => r.Name).Length(4, 60);
+            RuleFor(r => r.Code).Length(5);
+        }
     }
 }
