9735542b84aaa743baf9bb704b6e532aec881706
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2019-01-20 21:03:30 +0300

    Final

--- KatlaSport.DataAccess/Migrations/201901142045062_AddProductDescriptionManCodePrice.cs
+++ KatlaSport.DataAccess/Migrations/201901142045062_AddProductDescriptionManCodePrice.cs
@@ -2,7 +2,7 @@ namespace KatlaSport.DataAccess.Migrations
 {
     using System.Data.Entity.Migrations;
 
-    /// <inheritdoc cref="DbMigration" />
+    /// <inheritdoc cref="DbMigration"/>
     public partial class AddProductDescriptionManCodePrice : DbMigration
     {
         public override void Up()
