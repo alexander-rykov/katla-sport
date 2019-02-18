10de6df468dfd32849d981a805a6700132114e67
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2019-02-19 00:26:39 +0300

    Add HiveSectionProductListComponent

---
+++ KatlaSportNg/src/app/hive-management/lists/hive-section-product-list/hive-section-product-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hive-section-product-list',
+  templateUrl: './hive-section-product-list.component.html',
+  styleUrls: ['./hive-section-product-list.component.css']
+})
+export class HiveSectionProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
