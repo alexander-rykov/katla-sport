f66f4021986a18aaa77bac474546f89410cd88f9
Katsiaryna Lapotka <katsiaryna_lapotka@epam.com>
2019-08-05 21:05:52 +0300

    Realize atep8

--- KatlaSportNg/src/app/hive-management/models/hive-section-list-item.ts
+++ KatlaSportNg/src/app/hive-management/models/hive-section-list-item.ts
@@ -1,4 +1,8 @@
 export class HiveSectionListItem {
     constructor(
+        public id: number,
+        public name: string,
+        public code: string,
+        public isDeleted: boolean
     ) { }
 }
--- KatlaSportNg/src/app/hive-management/models/hive-section.ts
+++ KatlaSportNg/src/app/hive-management/models/hive-section.ts
@@ -1,3 +1,5 @@
 export class HiveSection {
-    constructor() { }
+    constructor(
+    )
+    { }
 }
