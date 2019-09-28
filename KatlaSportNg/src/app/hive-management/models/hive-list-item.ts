517a4fbe84fda8b8b876848a623ba2f83cc9975b
Mikita Reut <Mikita_Reut@epam.com>
2019-09-28 19:25:07 +0300

    Merge step6. Add Code and Name columns to hive-list component.

--- KatlaSportNg/src/app/hive-management/models/hive-list-item.ts
+++ KatlaSportNg/src/app/hive-management/models/hive-list-item.ts
@@ -2,6 +2,7 @@ export class HiveListItem {
     constructor(
         public id: number,
         // TODO STEP 6 - Add parameters name and code here.
+        
         public name: string,
         public code: string,
         public isDeleted: boolean
